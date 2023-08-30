d2182b86ed0c48f3219b2b96bf0ef35c356f6f16
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-08-30 12:54:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/iwhiwh/DQ-the-file-donor /DQ-the-file-donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
-cd /DQTheFileDonorBot
+cd /DQ-the-file-donor
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
